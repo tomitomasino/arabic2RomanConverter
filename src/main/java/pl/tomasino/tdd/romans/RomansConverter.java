b9134aa286e346b58424351f8213c6530f257f00
Tomek Rucinski <tomasz.rucinski@gmail.com>
2018-05-25 15:22:00 +0200

    remove TDD development history

--- src/main/java/pl/tomasino/tdd/romans/RomansConverter.java
+++ src/main/java/pl/tomasino/tdd/romans/RomansConverter.java
@@ -32,28 +32,6 @@ public class RomansConverter {
 			}
 		}
 
-		//		if (arabicNumb < 10 && ((arabicNumb + 1) % 5 == 0 || (arabicNumb + 1) % 10 == 0)) // for 4,9,14,19 etc
-		//			result = "I";
-		//
-		//		if (arabicNumb + 1 >= 10 && (arabicNumb % 10 >= 0 || (arabicNumb + 1) % 10 == 0)) // for 10, 20,30 etc
-		//			result += "X";
-		//
-		//		if (arabicNumb > 10 && ((arabicNumb + 1) % 5 == 0 || (arabicNumb + 1) % 10 == 0)) // for 4,9,14,19 etc
-		//			result += "I";
-		//
-		//		if (arabicNumb > 3 && (arabicNumb + 1) % 10 != 0 && !(arabicNumb % 10 == 0) && ((arabicNumb + 1) % 5 == 0 || // for 4
-		//				((arabicNumb % 5 >= 0 && arabicNumb % 5 <= 3) && arabicNumb % 10 >= 5))) // for 5-8
-		//			result += "V";
-		//
-		//		if (arabicNumb % 5 == 1) // for 1,6,11,16etc
-		//			result += "I";
-		//
-		//		if (arabicNumb % 5 == 2) // for 2,7,12,17 etc
-		//			result += "II";
-		//
-		//		if (arabicNumb % 5 == 3) // for 3,8,13,18
-		//			result += "III";
-
 		return result;
 	}
 
